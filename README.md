8b25a540340a97230bf7d6d377fc5557bf6a4e96
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-01 17:52:25 -0500

    added user questions

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ https://tedheikkila.github.io/hw9-readme-generator/
 
 Video Recording:
 <br>
+
+Screenshots:
+<br>
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: 'input', message: 'What is your GitHub username?', name: 'username'},
+    {type: 'input', message: 'What is your email address?', name: 'email'},
+    {type: 'input', message: "What is your project's name?", name: 'project'},
+    {type: 'input', message: 'Write a short description of project:', name: 'description'},
+    {type: 'input', message: 'Enter in type of license for project:', name: 'license'},
+    {type: 'input', message: 'What command to use to install dependencies?', name: 'install'},
+    {type: 'input', message: 'What command to use to run tests?', name: 'tests'},
+    {type: 'input', message: 'What does user need to know about repo?', name: 'info'},
+    {type: 'input', message: 'What does user need to know to contribute?', name: 'contribute'}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer 
+    .prompt(questions)
+}
 
 // Function call to initialize app
 init();
